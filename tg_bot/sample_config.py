66bbd1a216ba96c20917d438e2c318a27ed26a38
Hajime25 <44037068+Hajime25@users.noreply.github.com>
2018-10-29 15:52:36 +0800

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -33,7 +33,7 @@ class Config(object):
     STRICT_GBAN = False
     STRICT_GMUTE = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'AAQFABPj6NoyAAThPdEW0n7wKA1OAAIC'  # banhammer marie sticker
+    BAN_STICKER = 'AAQFABPj6NoyAAThPdEW0n7wKA1OAAIC'  # finish na sticker
     ALLOW_EXCL = False  # Allow ! commands as well as /
 
     # MEMES
